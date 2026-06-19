63b0622d709e63716f4ed389b48f1bd2caba5319
Rama Setyawan <rmstwn@gmail.com>
2026-06-19 22:27:05 +0900

    tes

--- tracker.py
+++ tracker.py
@@ -1,28 +1,11 @@
 import os
-import json
 import requests
 
-URL = "https://www.apple.com/jp/shop/refurbished/iphone"
-WEBHOOK = os.environ["DISCORD_WEBHOOK"]
+WEBHOOK_URL = os.environ["DISCORD_WEBHOOK"]
 
-html = requests.get(URL, timeout=30).text
-
-found = "iPhone 15 Pro" in html
-
-try:
-    with open("cache.json", "r") as f:
-        cache = json.load(f)
-except:
-    cache = {"found": False}
-
-if found and not cache["found"]:
-    requests.post(
-        WEBHOOK,
-        json={
-            "content":
-            f"🚨 iPhone 15 Pro ditemukan!\n{URL}"
-        }
-    )
-
-with open("cache.json", "w") as f:
-    json.dump({"found": found}, f)
+requests.post(
+    WEBHOOK_URL,
+    json={
+        "content": "🚨 Test message!"
+    }
+)
